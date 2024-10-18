68fdbf222f52a2cdbd623f8200b37f28a0c7f77a
Hasiitha <bandarahasitha68@gmail.com>
2024-10-18 16:25:04 +0530

    chore[1]:initialized project

---
+++ src/main/java/com/hasitha/employee_initial_web_app/EmployeeInitialWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.hasitha.employee_initial_web_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeInitialWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeInitialWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hasitha/employee_initial_web_app/EmployeeInitialWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hasitha.employee_initial_web_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeInitialWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
